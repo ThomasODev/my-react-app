e137375a7e85b83d5f17029f2974248c8963abcb
ThomasODev <thomas.ottone@gmail.com>
2018-01-28 12:36:37 +0100

    Improving the main menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Randomly generate a number. Then this will change the input text.
+ * @type {number}
+ */
+const math = Math.random() * 100
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                {
+                    math > 50 ?
+                        <p className="App-intro">
+                            To get started, edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        :
+                        <p className="App-intro">
+                            You won't start from anywhere, you'll have to lean the code yourself. Goodluck.
+                        </p>
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
